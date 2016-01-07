4edce9e818c1af5b11feaca93a139ae24faddbc7
Michael Bruenisholz <info@michael-bruenisholz.ch>
2016-01-07 23:05:29 +0100

    added the fade function, and some kind of state files

--- light.py
+++ light.py
@@ -30,9 +30,37 @@ scenetype = sys.argv[1]
 #Arg2 - Scenefile
 scenefile = sys.argv[2]
 
+#Write Channel Function
+def setchannel( channel, value ):
+	pwm.setPWM(channel, 0, value)
+	statefile = 'state/' + str(channel) + '.state'
+	filehandle = open(statefile, "w")
+	current_value = str(value)
+	filehandle.write(current_value)
+	filehandle.close()
+
+#Fade-Function
+def fadefromto( channel, from_red, from_green, from_blue, to_red, to_green, to_blue, steps, delay ):
+	red_step = float(to_red - from_red)/steps
+	green_step = float(to_green - from_green)/steps
+	blue_step = float(to_blue - from_blue)/steps
+	channel = str(channel)
+	chred = eval('ch' + channel + 'r')
+	chgreen = eval('ch' + channel + 'g')
+	chblue = eval('ch' + channel + 'b')
+	for step in range(0, steps+1):
+        	red_value = int(from_red + red_step*step)
+        	green_value = int(from_green + green_step*step)
+        	blue_value = int(from_blue + blue_step*step)
+        	pwm.setPWM(chred, 0, red_value)
+        	pwm.setPWM(chgreen, 0, green_value)
+        	pwm.setPWM(chblue, 0, blue_value)
+		time.sleep(delay)
+
+#fadefromto( 3, 0, 0, 0, 4000, 4000, 4000, 20, 0.1 )
+
 #Path to Scenefile
 scenepath = 'scenes-' + scenetype + '/' + scenefile + '.scn'
-print scenepath
 
 #read Scene-File into array
 array = open(scenepath).read().split('\n')
@@ -55,21 +83,21 @@ val5g = int(array[13])
 val5b = int(array[14])
 
 #set all channels to values
-pwm.setPWM(ch1r, 0, val1r)
-pwm.setPWM(ch1g, 0, val1g)
-pwm.setPWM(ch1b, 0, val1b)
-pwm.setPWM(ch2r, 0, val2r)
-pwm.setPWM(ch2g, 0, val2g)
-pwm.setPWM(ch2b, 0, val2b)
-pwm.setPWM(ch3r, 0, val3r)
-pwm.setPWM(ch3g, 0, val3g)
-pwm.setPWM(ch3b, 0, val3b)
-pwm.setPWM(ch4r, 0, val4r)
-pwm.setPWM(ch4g, 0, val4g)
-pwm.setPWM(ch4b, 0, val4b)
-pwm.setPWM(ch5r, 0, val5r)
-pwm.setPWM(ch5g, 0, val5g)
-pwm.setPWM(ch5b, 0, val5b)
+setchannel(ch1r, val1r)
+setchannel(ch1g, val1g)
+setchannel(ch1b, val1b)
+setchannel(ch2r, val2r)
+setchannel(ch2g, val2g)
+setchannel(ch2b, val2b)
+setchannel(ch3r, val3r)
+setchannel(ch3g, val3g)
+setchannel(ch3b, val3b)
+setchannel(ch4r, val4r)
+setchannel(ch4g, val4g)
+setchannel(ch4b, val4b)
+setchannel(ch5r, val5r)
+setchannel(ch5g, val5g)
+setchannel(ch5b, val5b)
 
 
 #time.sleep(3)
